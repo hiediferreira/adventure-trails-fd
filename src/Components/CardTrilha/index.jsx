3f3886925ad889e97852383ff5e2936736f33efe
Hiédi Ferreira <hiedyferreira@gmail.com>
2024-04-02 10:39:24 -0300

    Personalizando a aplicação

--- src/Components/CardTrilha/index.jsx
+++ src/Components/CardTrilha/index.jsx
@@ -6,11 +6,19 @@ function CardTrilha({dadosTrilha}){
     return(
         <div className="card-container">
 
-            <img className="card-imagem" width={"200px"} src={dadosTrilha.urlImagem} alt="imagem-trilha" /> 
-            <h1>{dadosTrilha.nomeTrilha}</h1>
-            <span>{dadosTrilha.cidade}/{dadosTrilha.estado}</span>
-     
-        
+            <img className="card-imagem" width={"200px"} src={dadosTrilha.urlImagem} alt="imagem-trilha" />
+
+            <div className="card-container-dados">
+                <p className="card-titulo">{dadosTrilha.nomeTrilha} - {dadosTrilha.cidade}/{dadosTrilha.estado}</p>
+                <p className="card-nome">Por: {dadosTrilha.nomeUsuario}</p>
+                <p className="card-duracao">Duração: {dadosTrilha.duracao} min</p>
+                <p className="card-trajeto">Trajeto: {dadosTrilha.trajeto} km</p>
+                <div className="card-dif">
+                    <p>{dadosTrilha.dificuldade}</p>
+                </div>
+                <a href="#" className="link-aval">Avaliações</a>
+            </div>
+
         </div>
     )
 }
