a375f1ef3a65bb184e9084d375970974ee37c60f
Hiédi Ferreira <hiedyferreira@gmail.com>
2024-04-08 14:27:37 -0300

    Aprimoramento do estilo

--- src/App.jsx
+++ src/App.jsx
@@ -9,15 +9,39 @@ function App() {
       cidade: "Florianópolis",
       estado: "SC",
       duracao: 120,
-      trajeto: 4,
+      trajeto: 5.75,
+      dificuldade: "Média",
+      tipo: "Caminhada",
+      nomeUsuario: "Hiédi",
+      urlImagem: "./src/assets/Costa-lagoa.jpeg"
+      //urlImagem: ".\src\assets\Costa-lagoa.jpeg"
+      //urlImagem: "https://images.pexels.com/photos/917510/pexels-photo-917510.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+    },
+    {
+      nomeTrilha: "Trilha do Poção",
+      cidade: "Florianópolis",
+      estado: "SC",
+      duracao: 25,
+      trajeto: 0.7,
       dificuldade: "Iniciante",
       tipo: "Caminhada",
       nomeUsuario: "Hiédi",
-      //urlImagem: "./assets/imagem-trilha.jpg"
-      urlImagem: "https://images.pexels.com/photos/917510/pexels-photo-917510.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+      urlImagem: "./src/assets/trilha-pocao.jpeg"
     },
     {
-      nomeTrilha: "Trilha da Costa da Lagoa",
+      nomeTrilha: "Trilha do Canto dos Ingleses",
+      cidade: "Florianópolis",
+      estado: "SC",
+      duracao: 50,
+      trajeto: 2.5,
+      dificuldade: "Iniciante",
+      tipo: "Caminhada",
+      nomeUsuario: "Hiédi",
+      urlImagem: "./src/assets/trilha-ingleses.jpeg"
+      //urlImagem: "https://images.pexels.com/photos/917510/pexels-photo-917510.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+    },
+    {
+      nomeTrilha: "Trilha da costa da Lagoa",
       cidade: "Florianópolis",
       estado: "SC",
       duracao: 120,
@@ -25,7 +49,6 @@ function App() {
       dificuldade: "Iniciante",
       tipo: "Caminhada",
       nomeUsuario: "Hiédi",
-      //urlImagem: "./assets/imagem-trilha.jpg"
       urlImagem: "https://images.pexels.com/photos/917510/pexels-photo-917510.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
     }
   ]
--- src/Components/CardTrilha/index.jsx
+++ src/Components/CardTrilha/index.jsx
@@ -1,24 +1,43 @@
 import * as PropTypes from "prop-types"
 import "./style.css"
 
+import {FaStar, FaRegHeart, FaHeart} from "react-icons/fa"
+
+// <FaHeart color={"red"} />  Coração vermelho
+
+
 function CardTrilha({dadosTrilha}){
 
     return(
         <div className="card-container">
-
             <img className="card-imagem" width={"200px"} src={dadosTrilha.urlImagem} alt="imagem-trilha" />
 
             <div className="card-container-dados">
-                <p className="card-titulo">{dadosTrilha.nomeTrilha} - {dadosTrilha.cidade}/{dadosTrilha.estado}</p>
+                <p className="card-titulo">
+                    {dadosTrilha.nomeTrilha} - {dadosTrilha.cidade}/{dadosTrilha.estado}
+                    <FaRegHeart style={{marginLeft:"10px"}}/>
+                </p>
+                
                 <p className="card-nome">Por: {dadosTrilha.nomeUsuario}</p>
-                <p className="card-duracao">Duração: {dadosTrilha.duracao} min</p>
-                <p className="card-trajeto">Trajeto: {dadosTrilha.trajeto} km</p>
-                <div className="card-dif">
-                    <p>{dadosTrilha.dificuldade}</p>
-                </div>
-                <a href="#" className="link-aval">Avaliações</a>
-            </div>
 
+                <p className="card-duracao">Duração: {dadosTrilha.duracao} min</p>
+
+                <p className="card-trajeto">Trajeto: {dadosTrilha.trajeto} km</p>
+                {/* <p> <FaHeart color={"red"} /></p> */}
+
+                <p className="card-dif">{dadosTrilha.dificuldade}</p>
+
+                <div className="card-aval">                    
+                    <div className="estrelas">
+                        <FaStar color={"rgb(149,4,149)"} className="estrelas" />
+                        <FaStar color={"rgb(149,4,149)"} className="estrelas" />
+                        <FaStar color={"rgb(149,4,149)"} className="estrelas" />
+                        <FaStar color={"rgb(149,4,149)"} />
+                    </div>
+                                       
+                    <a href="#" className="link-aval">Avaliações</a>
+                </div>
+            </div>
         </div>
     )
 }
